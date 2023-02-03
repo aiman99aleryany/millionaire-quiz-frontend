7861260c2a6ed2d3a47cf1c3c3c4cdc2e46047ed
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-02-03 16:56:55 +0200

    Update: Quiz component is finished

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -2,12 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import './Quiz.scss';
 
-function Quiz({ questions, setTimeOut, questionNr, setQuestionNr }) {
+function Quiz({ questions, setStop, questionNr, setQuestionNr }) {
     const [question, setQuestion] = useState(null);
+    const [selectedAnswer, SetSelectedAnswer] = useState(null);
+    const [className, setClassName] = useState('answer');
 
     useEffect(() => {
         setQuestion(questions[questionNr - 1]);
     }, [questions, questionNr]);
+
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback();
+        }, duration);
+    };
+
+    const handleClick = (answer) => {
+        SetSelectedAnswer(answer);
+        setClassName('answer active');
+
+        delay(3000, () =>
+            setClassName(answer.correct ? 'answer correct' : 'answer wrong')
+        );
+
+        delay(6000, () => {
+            if (answer.correct) {
+                setQuestionNr((state) => state + 1);
+                SetSelectedAnswer(null);
+            } else {
+                setStop(true);
+            }
+        });
+    };
 
     return (
         <section className="quiz">
@@ -15,7 +41,13 @@ function Quiz({ questions, setTimeOut, questionNr, setQuestionNr }) {
             <div className="answers">
                 {question?.answers.map((answer) => {
                     return (
-                        <div className="answer" key={nanoid()}>
+                        <div
+                            className={
+                                selectedAnswer === answer ? className : 'answer'
+                            }
+                            key={nanoid()}
+                            onClick={() => handleClick(answer)}
+                        >
                             {answer.text}
                         </div>
                     );
